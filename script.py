6b6f3c1ccf9c9fdf2d35723ffcd470ec894f3482
Yvette-Manishimwe <yvettemanishimwe9@gmail.com>
2024-02-11 22:25:57 +0200

    provided brief explanation on each line of codes

--- script.py
+++ script.py
@@ -1,23 +1,32 @@
-import cv2
-import numpy as np
-import pytesseract
+# Import necessary libraries
+import cv2  # OpenCV library for computer vision tasks
+import numpy as np  # NumPy library for numerical operations
+import pytesseract  # Tesseract OCR library for text extraction from images
 
+# Set the path to the Tesseract executable
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-# load image, grayscale, apply sharpening filter, otsu's threshold 
+# Read the image using OpenCV
 image = cv2.imread('meter.png')
+
+# Convert the image to grayscale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Define a sharpening kernel for image enhancement
 sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+
+# Apply the sharpening kernel to the grayscale image
 sharpen = cv2.filter2D(gray, -1, sharpen_kernel)
+
+# Apply Otsu's thresholding to create a binary image
 thresh = cv2.threshold(sharpen, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-# ocr
+# Perform OCR on the thresholded image to extract text
 data = pytesseract.image_to_string(thresh, lang='eng', config='--psm 6')
 
-# cv2.imshow('sharpen', sharpen)
-# cv2.imshow('thresh', thresh)
-# cv2.waitKey()
-numbers = ''.join(filter(str.isdigit, data))
+# Extract only numeric characters from the OCR result
+nums = ''.join(filter(str.isdigit, data))
 
-# Print extracted numbers
-print("Extracted Numbers:", numbers)
+# Print the extracted numeric characters
+print("Extracted numbers:", nums)
+
